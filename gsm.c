f1c9f80be99c9389572c0e36a99418f70cbf9dd4
Hector-Tosado <hector.tosado@upr.edu>
2013-04-07 04:18:27 -0400

    Connection to TCP/IP from GSM to Server.
    Steps forward into adding files to the DB from the hardware.

--- gsm.c
+++ gsm.c
@@ -12,10 +12,16 @@
 #include "string.h"
 #include "stdio.h"
 
+#define Camera		UART1_BASE
+#define GSM 		UART3_BASE
+#define GPS			UART4_BASE
+
 volatile char response[1000];
-int a;
+int a, connEstablished = 0;
 char cChar;
 
+volatile long int i=6000000;
+volatile long int seg=6000000;//3000000 funciona con 38400 baudrate.
 //*****************************************************************************
 //
 // Send a string to the UART.
@@ -90,136 +96,221 @@ UARTReceive(unsigned long ulBase, volatile char pucBuffer[])
 // The UART1 interrupt handler.
 //
 //****************************************************************************
+//void
+//UART1IntHandler(void)
+//{
+//	unsigned long ulStatus;
+//	//tBoolean bRc;
+//
+//	// Get the interrupt status.
+//	ulStatus = UARTIntStatus(UART1_BASE, true);
+//
+//	// Clear the asserted interrupts.
+//	UARTIntClear(UART1_BASE, ulStatus);
+//
+//	// Mientras haya data en el UART1
+//	UARTReceive(UART1_BASE, response);
+//
+//}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//*****************************************************************************
+//
+// The UART3 interrupt handler.
+//
+//****************************************************************************
 void
-UART1IntHandler(void)
+UART3IntHandler(void)
 {
 	unsigned long ulStatus;
 	//tBoolean bRc;
 
 	// Get the interrupt status.
-	ulStatus = UARTIntStatus(UART3_BASE, true);
+	ulStatus = UARTIntStatus(GSM, true);
 
 	// Clear the asserted interrupts.
-	UARTIntClear(UART3_BASE, ulStatus);
+	UARTIntClear(GSM, ulStatus);
 
 	// Mientras haya data en el UART1
-	UARTReceive(UART3_BASE, response);
+	UARTReceive(GSM, response);
 
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////
+////*****************************************************************************
+////
+//// The UART4 interrupt handler.
+////
+////****************************************************************************
+//void
+//UART4IntHandler(void)
+//{
+//	unsigned long ulStatus;
+//	//tBoolean bRc;
+//
+//	// Get the interrupt status.
+//	ulStatus = UARTIntStatus(UART4_BASE, true);
+//
+//	// Clear the asserted interrupts.
+//	UARTIntClear(UART4_BASE, ulStatus);
+//
+//	// Mientras haya data en el UART1
+//	UARTReceive(UART4_BASE, response);
+//
+//}
 /*
  * Setup FTP Connection
  * Configuration, Activates Context, Opens server connection, File transfer type,
  */
 void
-FTPConnect(void)
+TCPConnect()
 {
-	/*INITIAL CONFIGURATION
+	connEstablished = 0;
+
+	/*INITIAL Configuration
 	 *Creates context 1.
-	 *APN: internet.claropr
+	 *APN: carspy.ece.uprm.edu
 	 *IP address: dynamically assigned by the ISP
 	 *Packet Data Protocol type: Internet Protocol (IP)
 	 *Data compression: OFF (0)
 	 *Header compression: OFF (0)
 	 */
-	 
-	// i=seg;
-	// while(i--){}
-	// UARTSend((unsigned long)UART1_BASE, (unsigned char *) "AT+CGDCONT=1,IP,internet.claropr,0.0.0.0,0,0 <cr>",44);//52 with carspy.ece...
-	// UARTCharPut(UART1_BASE, 13);
-	// UARTCharPut(UART1_BASE, 26);
-	
-	//INITIAL CONFIGURATION
-	//The TCP/IP stack behavior must be configured by setting:
-	//The packet default size: 512
-	//The socket inactivity timeout: 90
-	//The connection timeout: 300
-	//The data sending timeout: 100
-
 	//	i=seg;
 	//	while(i--){}
-	//	UARTSend((unsigned long)UART1_BASE, (unsigned char *) "AT#SCFG=1,1,512,90,300,100", 26);
+	//	UARTSend((unsigned long)GSM, (unsigned char *) "AT+CGDCONT=1,IP,internet.claropr.com,0.0.0.0,0,0 <cr>",44);//52 with carspy.ece...
 	//	UARTCharPut(UART1_BASE, 13);
 	//	UARTCharPut(UART1_BASE, 26);
-	//
 
+
+	/* INITIAL CONFIGURATION
+	 * The TCP/IP stack behavior must be configured by setting:
+	 * The packet default size: 512
+	 * The socket inactivity timeout: 90
+	 * The connection timeout: 300
+	 * The data sending timeout: 100
+	 */
+
+	//		i=seg;
+	//		while(i--){}
+	//		UARTSend((unsigned long)GSM, (unsigned char *) "AT#SCFG=1,1,512,90,300,100 <cr> <lf>", 36);
+	//		UARTCharPut(UART1_BASE, 13);
+	//		UARTCharPut(UART1_BASE, 26);
+
+	/*
+	 * Check assigned context Id's. NOT WORKING ATM
+	 */
+	//		i=seg;
+	//		while(i--){}
+	//		UARTSend((unsigned long)GSM, (unsigned char *) "AT#CGDCONT=?", 12);
+	//		UARTCharPut(UART1_BASE, 13);
+	//		UARTCharPut(UART1_BASE, 26);
+
+	/*When enabled, device related errors cause the +CME ERROR: <err>
+	 * final result code instead of the default ERROR final result code.
+	 * ERROR is anyway returned normally when the error message is
+	 * related to syntax, invalid parameters, or DTE functionality
+	 */
+
+	//	i=seg;
+	//	while(i--){}
+	//	UARTSend((unsigned long)GSM, (unsigned char *) "AT+CMEE=1", 9);
+	//	UARTCharPut(GSM, 13);
+	//	UARTCharPut(GSM, 26);
+
+	/*
+	 * Return activated context.
+	 */
+	//	i=seg;
+	//	while(i--){}
+	//	UARTSend((unsigned long)GSM, (unsigned char *) "AT+CGDCONT?", 11);
+	//	UARTCharPut(GSM, 13);
+	//	UARTCharPut(GSM, 26);
+
+	/*
+	 * Returns IP Assigned by Claro for GSM for Client service.
+	 */
+	//	i=seg;
+	//	while(i--){}
+	//	UARTSend((unsigned long)GSM, (unsigned char *) "AT#CGPADDR=1", 12);
+	//	UARTCharPut(GSM, 13);
+
+	/*
+	 * SELINT is 2.
+	 */
+	//		i=seg;
+	//		while(i--){}
+	//		UARTSend((unsigned long)GSM, (unsigned char *) "AT#SELINT?", 10);
+	//		UARTCharPut(GSM, 13);
+	//		UARTCharPut(GSM, 26);
 	/*
 	 * Activate context defined in AT+CGDCONT. Returns IP assigned by network.
+	 * 0 - Disactivate | 1 - Activate Context
 	 */
-	//	i=seg;
-	//	while(i--){}
-	//	UARTSend((unsigned long)UART3_BASE, (unsigned char *) "AT#SGACT=1,1", 12);
-	//	UARTCharPut(UART3_BASE, 13);
-	//	UARTCharPut(UART3_BASE, 26);
-
+//		i=seg;
+//		while(i--){}
+//		UARTSend((unsigned long)GSM, (unsigned char *) "AT#SGACT=1,1", 12);
+//		UARTCharPut(GSM, 13);
+////	DELAY AQUI PARA PODER LEER EL IP BIEN.
+//			SysCtlDelay(100000000);
 	/*
-	 * Open FTP Connection
-	 * server:port - Port defaults 21.
-	 * Username and Password - Needed for server authentication.
-	 * 0 - Active mode.
+	 * FTP settings of time-out
 	 */
 	//	i=seg;
 	//	while(i--){}
-	//	UARTSend((unsigned long)UART3_BASE, (unsigned char *) "AT#FTPOPEN=carspy.ece.uprm.edu,capstone,thai4Ais,0", 50);
-	//	UARTCharPut(UART3_BASE, 13);
-	//	UARTCharPut(UART3_BASE, 26);
-
+	//	UARTSend((unsigned long)GSM, (unsigned char *) "AT#FTPTO=1000", 13);
+	//	UARTCharPut(GSM, 13);
+	//
 	/*
-	 * Set the file transfer type. 0-binary, 1-ASCII
+	 * Check Data Connection
 	 */
-	//	i=seg;
-	//	while(i--){}
-	//	UARTSend((unsigned long)UART3_BASE, (unsigned char *) "AT#FTPTYPE=1", 12);
-	//	UARTCharPut(UART3_BASE, 13);
-	//	UARTCharPut(UART3_BASE, 26);
+	//		i=seg;
+	//		while(i--){}
+	//		UARTSend((unsigned long)GSM, (unsigned char *) "AT#GPRS?", 8);
+	//		UARTCharPut(GSM, 13);
 
+
+	if(1){}
 	/*
-	 * FTP file transfer to the server.
-	 * Depends on a successful data connection.
+	 * SEND STREAM OF DATA HERE!
 	 */
-	//	i=seg;
-	//	while(i--){}
-	//	UARTSend((unsigned long)UART3_BASE, (unsigned char *) "AT#FTPPUT=file.txt", 19);
-	//	UARTCharPut(UART3_BASE, 13);
-	//	UARTCharPut(UART3_BASE, 26);
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
-int main(void)
+void
+UARTSetup()
 {
-	volatile long int i=1000000;
-	volatile long int seg=1000000;//3000000 funciona con 38400 baudrate.
-	// Setear clock
-	SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+	//UART Port configuration
 
-	//Configuracion de los puertos UART que se van a usar en el micro
-	//	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
-	//	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+	//SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+	//SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);//Terminal UART
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART3);
+	//	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART4);
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
 
-	// Enable processor interrupts
-	IntMasterEnable();
 
-	//Pin_map
-	// Configure the GPIO pin muxing for the UART function.
+	//Pin_map HELP
+	// Enable ports for the UART Rx or Tx (eg, Enable port PA0 for UART0 U0RX)
 	GPIOPinConfigure(0x00000001);//GPIO_PA0_U0RX
 	GPIOPinConfigure(0x00000401);//GPIO_PA1_U0TX
 	//	GPIOPinConfigure(0x00010001);//GPIO_PB0_U0RX
 	//	GPIOPinConfigure(0x00010401);//GPIO_PB1_U0TX
 	GPIOPinConfigure(0x00021801);//GPIO_PC6_U3RX
 	GPIOPinConfigure(0x00021C01);//GPIO_PC7_U3TX
+	//	GPIOPinConfigure(0x00021001);//GPIO_PC4_U4RX
+	//	GPIOPinConfigure(0x00021401);//GPIO_PC5_U4TX
 
-	// Since GPIO A0 and A1 are used for the UART function, they must be
+
+	// Configure the GPIOs (ex. A0, A1) that are used for the UART function, they must be
 	// configured for use as a peripheral function (instead of GPIO).
 	GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 	//	GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 	GPIOPinTypeUART(GPIO_PORTC_BASE, GPIO_PIN_6 | GPIO_PIN_7);
+	//	GPIOPinTypeUART(GPIO_PORTC_BASE, GPIO_PIN_5 | GPIO_PIN_4);
 
 	// Configure the UART for 115,200, 8-N-1 operation.
 	UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
@@ -228,42 +319,134 @@ int main(void)
 	//	UARTConfigSetExpClk(UART1_BASE, SysCtlClockGet(), 115200,
 	//			(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
 	//					UART_CONFIG_PAR_NONE));
-	UARTConfigSetExpClk(UART3_BASE, SysCtlClockGet(), 115200,
+	UARTConfigSetExpClk(GSM, SysCtlClockGet(), 115200,
 			(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
 					UART_CONFIG_PAR_NONE));
+}
+void
+checkConnection()
+{
+	/*
+	 * Returns OK if carrier if GSM is correctly connected.
+	 * Returns NO CARRIER otherwise.
+	 */
 
-
-	//	IntEnable(INT_UART1);
-	//	UARTIntEnable(UART1_BASE, UART_INT_RX | UART_INT_RT);
-
-	IntEnable(INT_UART3);
-	UARTIntEnable(UART3_BASE, UART_INT_RX | UART_INT_RT);
-	// Put a character to show start of example. This will display on the terminal.
-	UARTCharPut(UART0_BASE, '!');
-	//strlen((const char*)command)
-	// Send to UART0 using UARTSend. Testing purposes.
 	i=seg;
 	while(i--){}
-	UARTSend((unsigned long)UART3_BASE, (unsigned char *) "AT", 2);
-	UARTCharPut(UART3_BASE, 13);
-	UARTCharPut(UART3_BASE, 26);
+	UARTSend((unsigned long)GSM, (unsigned char *) "AT", 2);
+	UARTCharPut(GSM, 13);
+	UARTCharPut(GSM, 26);
 
-	FTPConnect();
+	i=seg;
+	while(i--){}
+
+	while(!connEstablished)
+	{
+		/*
+		 * Expected 0,1 if registered in network. Re-try until this is 0,1.
+		 */
+		UARTSend((unsigned long)GSM, (unsigned char *) "AT+CREG?", 8);
+		UARTCharPut(GSM, 13);
+		UARTCharPut(GSM, 26);
+
+		SysCtlDelay(10000000);
+
+		if((response[a-11] == '0') && (response[a-9] == '1'))
+		{
+			connEstablished = 1;
+			UARTCharPut(UART0_BASE, response[a-11]);
+		}
+
+	}
+
+
+
+
+}
+int main(void)
+{
+
+	//Set Clock for MCU to user. 50MHz
+	SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+	//Setup all UART peripherals to be used.
+	UARTSetup();
+	// Enable processor interrupts
+	IntMasterEnable();
+	IntEnable(INT_UART3);
+	UARTIntEnable(GSM, UART_INT_RX | UART_INT_RT);
+
+	// Put a character to show start of example. This will display on the terminal.
+	UARTCharPut(UART0_BASE, '!');
+
+	//Verify GSM is connected to carrier.
+
+	checkConnection();
+	SysCtlDelay(10000000);
+	if(connEstablished == 1)
+	{
+		TCPConnect();
+	}
+
+	//Check available ports using lsof -pn | grep LISTEN
+	i=seg;
+	while(i--){}
+	UARTSend((unsigned long)GSM, (unsigned char *) "AT#SD=1,0,6102,carspy.ece.uprm.edu", 34);
+	UARTCharPut(GSM, 13);
+
+	//Delay to assure socket connection.
+	SysCtlDelay(50000000);
+
+	a = 0;
+	if(response[a] == '0')
+	{
+
+	}
+	else
+	{
+
+	}
+
+
+	i=seg;
+	while(i--){}
+	int l = strlen((const char*)response);
+	//Change to photo after program is integrated.
+	UARTSend((unsigned long)GSM, (unsigned char *)response, l);
+	UARTCharPut(GSM, 13);
+	UARTCharPut(GSM, 26);
+
+
+	/*
+	 * OR SEND STREAM OF DATA HERE.
+	 */
+	/*
+	 * HAS to be sent after a connection has been established. It requires +++ to exit Data Traffic Mode (Socket Mode)
+	 */
+	//		i=seg;
+	//		while(i--){}
+	//		UARTSend((unsigned long)UART1_BASE, (unsigned char *) "+++",3);
+	//		SysCtlDelay(100000000); //Needs 2 second delay between the command and the LF and CR characters for correct reading of pause command.
+	//		UARTCharPut(UART1_BASE, 13);
+	//		UARTCharPut(UART1_BASE, 26);
+
+	// AFTER +++ #SO continues connection.
+
+
 	/*
 	 * Prints out possible baud rates.
 	 */
 	//	i=seg;
 	//	while(i--){}
-	//	UARTSend((unsigned long)UART3_BASE, (unsigned char *) "AT+IPR=?",8);
-	//	UARTCharPut(UART3_BASE, 13);
-	//	UARTCharPut(UART3_BASE, 26);
+	//	UARTSend((unsigned long)GSM, (unsigned char *) "AT+IPR=?",8);
+	//	UARTCharPut(GSM, 13);
+	//	UARTCharPut(GSM, 26);
 
 	/*
 	 * The minimum quality of service requested parameters represent the
 	 * boundary under which the connection quality is not anymore
 	 * acceptable and will be terminated
 	 * CID: 1
-	 * Precedence: 0 (subcribed) Applied when network has a heavy duty and user precedence must be followed.
+	 * Precedence: 0 (subscribed) Applied when network has a heavy duty and user precedence must be followed.
 	 * Delay: 0 Maximum allowable time delay class between the sending and the reception of a packet
 	 * Reliability: 0 connection reliability requested, the higher is the number the less reliable is the data exchanged
 	 * Peak: 0 Peak data transfer throughput
@@ -296,15 +479,7 @@ int main(void)
 	//
 	//	SysCtlDelay(200000000);
 
-	/*
-	 * HAS to be sent after a connection has been established. It requires +++ to exit Data Traffic Mode (Socket Mode)
-	 */
-	//		i=seg;
-	//		while(i--){}
-	//		UARTSend((unsigned long)UART1_BASE, (unsigned char *) "+++",3);
-	//		SysCtlDelay(100000000); //Needs 2 second delay between the command and the LF and CR characters for correct reading of pause command.
-	//		UARTCharPut(UART1_BASE, 13);
-	//		UARTCharPut(UART1_BASE, 26);
+
 
 
 	while(1){}
